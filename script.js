828c21a210451ca53658f36ddc72c5dd29029236
Cap'n Kleffa <kleffa@gmail.com>
2023-06-12 21:15:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//script.js
+window.addEventListener('DOMContentLoaded', () => {
+    const videoElement = document.getElementById('video-background');
+
+    // Verificar si el navegador es compatible con getUserMedia
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then((stream) => {
+                // Asignar el stream de la cámara al elemento de video
+                videoElement.srcObject = stream;
+            })
+            .catch((error) => {
+                console.error('Error al acceder a la cámara: ', error);
+            });
+    } else {
+        console.error('getUserMedia no es soportado por el navegador');
+    }
+});
