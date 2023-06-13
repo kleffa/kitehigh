232143372f1657f9f8967ae55ed711d5ba464c6e
Cap'n Kleffa <kleffa@gmail.com>
2023-06-12 21:47:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
     const videoElement = document.getElementById('video-background');
-    const captureButton = document.getElementById('capture-button');
     const canvasElement = document.getElementById('canvas');
     const context = canvasElement.getContext('2d');
 
@@ -12,6 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
             .then((stream) => {
                 mediaStream = stream;
                 videoElement.srcObject = mediaStream;
+                videoElement.play();
             })
             .catch((error) => {
                 console.error('Error al acceder a la cámara: ', error);
@@ -20,18 +20,16 @@ window.addEventListener('DOMContentLoaded', () => {
         console.error('getUserMedia no es soportado por el navegador');
     }
 
-    captureButton.addEventListener('click', () => {
-        if (mediaStream) {
-            // Capturar imagen del video y mostrarla en el canvas
+    videoElement.addEventListener('play', () => {
+        const drawFrame = () => {
+            if (videoElement.paused || videoElement.ended) {
+                return;
+            }
+
             context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+            requestAnimationFrame(drawFrame);
+        };
 
-            // Obtener la imagen como base64
-            const imageData = canvasElement.toDataURL('image/jpeg');
-
-            // Mostrar la imagen capturada (puedes ajustar esto según tus necesidades)
-            const imageElement = new Image();
-            imageElement.src = imageData;
-            document.body.appendChild(imageElement);
-        }
+        requestAnimationFrame(drawFrame);
     });
 });
