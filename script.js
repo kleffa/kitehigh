bccb06fd8357dd1e4b51449c18144a3816c33382
Cap'n Kleffa <kleffa@gmail.com>
2023-06-12 21:44:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//script.js
 window.addEventListener('DOMContentLoaded', () => {
     const videoElement = document.getElementById('video-background');
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+    const captureButton = document.getElementById('capture-button');
+    const canvasElement = document.getElementById('canvas');
+    const context = canvasElement.getContext('2d');
 
+    let mediaStream;
 
     // Verificar si el navegador es compatible con getUserMedia
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
             .then((stream) => {
-                // Asignar el stream de la cámara al elemento de video
-                videoElement.srcObject = stream;
+                mediaStream = stream;
+                videoElement.srcObject = mediaStream;
             })
             .catch((error) => {
                 console.error('Error al acceder a la cámara: ', error);
@@ -17,4 +19,19 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
         console.error('getUserMedia no es soportado por el navegador');
     }
+
+    captureButton.addEventListener('click', () => {
+        if (mediaStream) {
+            // Capturar imagen del video y mostrarla en el canvas
+            context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+
+            // Obtener la imagen como base64
+            const imageData = canvasElement.toDataURL('image/jpeg');
+
+            // Mostrar la imagen capturada (puedes ajustar esto según tus necesidades)
+            const imageElement = new Image();
+            imageElement.src = imageData;
+            document.body.appendChild(imageElement);
+        }
+    });
 });
